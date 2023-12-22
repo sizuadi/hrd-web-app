69ac064c13da411f427bbdf67d039026ed47525c
Adi Siswanto <adi@atomic.id>
2023-12-22 17:10:53 +0700

    fix app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,7 @@
+import { Livewire } from "../../vendor/livewire/livewire/dist/livewire.esm";
 import "./bootstrap";
 import "../css/app.css";
 import flatpickr from "flatpickr";
 window.flatpickr = flatpickr;
-
-import {
-    Livewire,
-    Alpine,
-} from "../../vendor/livewire/livewire/dist/livewire.esm";
-
-Alpine.plugin();
 
 Livewire.start();
